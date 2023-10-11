df729f7601eb4fb330bff4081ed540cd72caf6bd
yuzakki <itsmehaassaann@gmail.com>
2023-10-11 14:09:46 +0100

    fix small fetching bug

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -69,12 +69,6 @@ export default function MovieDetails() {
       async function getMovieDetails() {
         setIsLoading(true);
 
-        // const res = await fetch(
-        //   `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
-        // );
-
-        // const data = await res.json();
-
         // Check if the current page is loaded over HTTPS or HTTP
         const protocol =
           window.location.protocol === "https:" ? "https" : "http";
