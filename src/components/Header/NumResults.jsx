098a9e8a3454ff6bcfe95fc5c1722b876a619569
yuzakki <itsmehaassaann@gmail.com>
2023-10-11 14:05:29 +0100

    fix a small bug

--- src/components/Header/NumResults.jsx
+++ src/components/Header/NumResults.jsx
@@ -5,7 +5,7 @@ export function NumResults() {
 
   return (
     <p className="num-results">
-      Found <strong>{movies.length}</strong> results
+      Found <strong>{movies?.length}</strong> results
     </p>
   );
 }
